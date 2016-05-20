af2e3699f3541b7c1a491287a2e339c490f314b8
LUCAS Maxime <maxime.lucas96@gmail.com>
2016-05-20 09:59:16 +0200

    Initialisation du projet Java

---
+++ src/main/java/fr/pilsdefutes/pils/de/futes/project/Application.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.pilsdefutes.pils.de.futes.project;
+
+/**
+ *
+ * @author Maxime
+ */
+public class Application {
+    public static void main(String[] argv)
+    {
+        System.out.println("Hello World !");
+    }
+}
