028201ba85635bc4e4f69bf67dffdd6e3f9a6284
LUCAS Maxime <maxime.lucas96@gmail.com>
2016-05-20 10:03:38 +0200

    Initialisation du MVC

---
+++ src/main/java/fr/pilsdefutes/pils/de/futes/project/controllers/Controller.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.pilsdefutes.pils.de.futes.project.controllers;
+
+/**
+ *
+ * @author Maxime
+ */
+public class Controller {
+    
+}
