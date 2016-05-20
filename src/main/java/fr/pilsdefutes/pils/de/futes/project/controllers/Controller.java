3810d6244adb0a531bb35f26c2f29a5eb50014fd
LUCAS Maxime <maxime.lucas96@gmail.com>
2016-05-20 16:55:44 +0200

    wip/Mise à jour des salles dans Controller

--- src/main/java/fr/pilsdefutes/pils/de/futes/project/controllers/Controller.java
+++ src/main/java/fr/pilsdefutes/pils/de/futes/project/controllers/Controller.java
@@ -5,75 +5,109 @@
  */
 package fr.pilsdefutes.pils.de.futes.project.controllers;
 
+import fr.pilsdefutes.pils.de.futes.project.models.Manutentionnaire;
 import fr.pilsdefutes.pils.de.futes.project.views.TcpGrandOrdonnateur;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
  * @author Maxime
  */
-public class Controller
-{
-	private String hote = null;
-	private int port = -1;
-	private TcpGrandOrdonnateur tcpGdOrdo = null;
-        private int nbLignes, nbCol;
-        private String configCave;
-        private int nbManutentionnaires, ordre;
+public class Controller {
 
-	public Controller(String hote, int port)
-        {
-		this.hote = hote;
-		this.port = port;
-		this.tcpGdOrdo = new TcpGrandOrdonnateur();
-	}
+    private String hote = null;
+    private int port = -1;
+    private TcpGrandOrdonnateur tcpGdOrdo = null;
+    private int nbLignes, nbCol;
+    private String configCave;
+    private int nbManutentionnaires, ordre;
 
-	public void scenario() throws IOException {
-		tcpGdOrdo.connexion(hote, port);
+    public Controller(String hote, int port) {
+        this.hote = hote;
+        this.port = port;
+        this.tcpGdOrdo = new TcpGrandOrdonnateur();
+    }
 
-                tcpGdOrdo.envoiChaine("Pils de futes");
-		tcpGdOrdo.envoiChaine("IUT Amiens");
-		
-                
-                
-		nbLignes = tcpGdOrdo.receptionEntier();
-                nbCol = tcpGdOrdo.receptionEntier();
-		
-                //configuration de la cave
-		configCave = tcpGdOrdo.receptionChaine();
+    public void scenario() throws IOException {
+        tcpGdOrdo.connexion(hote, port);
 
-                //nb de joueurs
-                nbManutentionnaires = tcpGdOrdo.receptionEntier();
-		
-                //ordre de jeu
-		ordre = tcpGdOrdo.receptionEntier();
-                
-                //Initialisation modèles TODO
-                Cave cave;
-                
-                while(true)
-                {
-                    for(int i = 0 ; i < ordre-1 ; i++)
-                        tcpGdOrdo.receptionChaine(); //TODO envoyer le résultat
-                    
-                    //PHASE TRAITEMENT
-                    String phraseDeJeu = "";
-                    
-                    ArrayList<Salle> sallesPotentielles = rechercheSalle(cave);
-                    
-                    tcpGdOrdo.envoiChaine(phraseDeJeu);
-                    
-                    
-                    for(int i = 0 ; i < nbManutentionnaires-ordre ; i++)
-                        tcpGdOrdo.receptionChaine(); //TODO envoyer le résultat
-                }
-		/*
+        tcpGdOrdo.envoiChaine("Pils de futes");
+        tcpGdOrdo.envoiChaine("IUT Amiens");
+
+        nbLignes = tcpGdOrdo.receptionEntier();
+        nbCol = tcpGdOrdo.receptionEntier();
+
+        //configuration de la cave
+        configCave = tcpGdOrdo.receptionChaine();
+
+        //nb de joueurs
+        nbManutentionnaires = tcpGdOrdo.receptionEntier();
+
+        //ordre de jeu
+        ordre = tcpGdOrdo.receptionEntier();
+
+        //Initialisation modèles TODO
+        Cave cave;
+
+        while (true) {
+            for (int i = 0; i < ordre - 1; i++) {
+                tcpGdOrdo.receptionChaine(); //TODO envoyer le résultat
+            }
+            //PHASE TRAITEMENT
+            String phraseDeJeu = "";
+
+            ArrayList<Salle> sallesPotentielles = rechercheSalle(cave);
+
+            tcpGdOrdo.envoiChaine(phraseDeJeu);
+
+            for (int i = 0; i < nbManutentionnaires - ordre; i++) {
+                tcpGdOrdo.receptionChaine(); //TODO envoyer le résultat
+            }
+        }
+        /*
 		// - déconnexion
 		try {
 			tcpGdOrdo.deconnexion();
 		} catch (IOException e) {
 			// Qu'importe si le serveur s'est arrêté avant le client.
 		} //*/
-	}
+    }
+
+    public void miseAJourSalles(String phrase, Manutentionnaire m) {
+        boolean continuer = true;
+        Pattern pattern = Pattern.compile("NESOPRI");
+        Matcher matcher;
+
+        for (int i = 0; i < phrase.length(); i++) {
+            char letter = phrase.charAt(i);
+
+            matcher = pattern.matcher(String.valueOf(letter));
+
+            if (matcher.find()) {
+                switch (letter) {
+                    case 'I':
+                        continuer = false;
+                        break;
+                    case 'N':
+                        
+                        break;
+                    case 'S':
+                        break;
+                    case 'E':
+                        break;
+                    case 'O':
+                        break;
+                    case 'R':
+                        break;
+                    case 'P':
+                        break;
+                }
+            }
+            
+            if( !continuer ) break;
+        }
+    }
 }
